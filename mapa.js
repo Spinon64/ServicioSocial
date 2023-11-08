c13bd9d66354780de8285e9854cd0659d75c44e6
Spinon64 <90027475+Spinon64@users.noreply.github.com>
2023-11-08 09:27:51 -0600

    push busqueda y agebs con personas

--- mapa.js
+++ mapa.js
@@ -1,78 +1,73 @@
+// ...
+
 // Crea un mapa Leaflet
 var map = L.map('map', {
     minZoom: 11   // Nivel de zoom mínimo permitido
 }).setView([-103.72714, 19.24997], 11);
+
 
 // Agrega una capa de mapa base
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Carga el archivo KML que contiene los polígonos
-fetch('Pruebas1.kml')
-    .then(response => response.text())
-    .then(kml => {
-        // Convierte el KML en una capa de polígonos en Leaflet
-        var polygons = omnivore.kml.parse(kml);
+var control = L.Control.geocoder({
+    position: 'topleft',
+    defaultMarkGeocode: true,
+    geocoder: L.Control.Geocoder.mapbox('pk.eyJ1Ijoic3Bpbm9uNjQiLCJhIjoiY2xvbjgwbmlqMHFmcDJrczZncGs1ejRnNCJ9._i-bNrmSZgkTubn6s64-cw', {
+        proximity: L.latLng(19.2433, -103.7242), // Centro de Colima
+       
+    }),
+}).addTo(map);
+
+control.on('markgeocode', function (e) {
+    map.setView(e.geocode.center, 15);
+});
+
+// Carga el archivo GeoJSON que contiene los polígonos
+fetch('Pruebas1.json')  // Reemplaza 'Pruebas1.geojson' con la URL de tu archivo GeoJSON
+    .then(response => response.json())  // Utiliza response.json() para analizar el archivo GeoJSON
+    .then(data => {
+        // Convierte el GeoJSON en una capa de polígonos en Leaflet
+        var polygons = L.geoJSON(data, {
+            style: function (feature) {
+                var personas = feature.properties.personas;
+                var nombre = feature.properties.name;
+                console.log(personas);
+                var fillColor;
 
 
-        // Define un objeto que mapea nombres de polígonos a colores
-        var colorMapping = {
-            'Mancha 1': 'red',
-            'Mancha 2': 'green',
-            'Mancha 3': 'blue'
-        };
+                 // Define el color basado en el valor de "personas"
+                if (nombre === "Municipio Colima") {
+                    fillColor = 'transparent'; // Establece el relleno como transparente
+                }
 
-        // Aplica el estilo personalizado basado en la categoría
-        polygons.setStyle(function (feature) {
-            var nombre = feature.properties.name;
-            var fillColor = colorMapping[nombre] || 'rgb(0,0,0,0)'; // Borde del mapa
+                if (personas === 3 && nombre === "060020001134A") {
+                    fillColor = 'black';
+                }
+                
+                if (personas <= 10){
+                    fillColor = 'purple';
+                } else if (personas <= 30 && personas >= 11) {
+                    fillColor = "green";
+                }
 
-            return {
-                fillColor: fillColor,
-                color: 'black',
-                weight: 2,
-                opacity: 1,
-                fillOpacity: 0.5
-            };
+                console.log(nombre)
+
+                return {
+                    fillColor: fillColor,
+                    color: 'blue',
+                    weight: 0.5,
+                    opacity: 1,
+                    fillOpacity: 0.5
+                };
+
+            }
         });
 
+      
 
-        // Función para habilitar/deshabilitar un polígono
-     function togglePolygon(polygonName) {
-        if (polygons) {
-            polygons.eachLayer(function (layer) {
-                if (layer.feature.properties.name === polygonName) {
-                    if (map.hasLayer(layer)) {
-                        map.removeLayer(layer);
-                    } else {
-                        map.addLayer(layer);
-                    }
-                }
-            });
-        }
-    }
-
-    // Agregar eventos de clic a los botones
-    document.getElementById('toggleMancha1').addEventListener('click', function () {
-        togglePolygon('0600200011091');
-    });
-
-    document.getElementById('toggleMancha2').addEventListener('click', function () {
-        togglePolygon('Mancha 2');
-    });
-
-    document.getElementById('toggleMancha3').addEventListener('click', function () {
-        togglePolygon('Mancha 3');
-    });
-
-    document.getElementById('toggleMunicipio').addEventListener('click', function () {
-        togglePolygon('Municipio Colima');
-    });
-
-
-
-
+     
 
         // Agrega los polígonos al mapa
         polygons.addTo(map);
@@ -80,5 +75,3 @@ fetch('Pruebas1.kml')
         // Ajusta la vista del mapa para que muestre todos los polígonos
         map.fitBounds(polygons.getBounds());
     });
-
-     
